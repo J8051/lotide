43ed58e84f8e6b54cd433d9e5db2b0e320c6d094
J8051 <nathaniel.jessica@yahoo.ca>
2022-11-17 22:16:19 -0500

    without function

---
+++ without.js
@@ -0,0 +1,13 @@
+
+const without = function(source, itemsToRemove){
+  let newArr =[];
+  for (let i = 0; i < source.length; i++) {
+    if(!itemsToRemove.includes(source[i])){
+      newArr.push(source[i]); 
+    } 
+  }
+  return newArr; 
+}
+console.log(without([1, 2, 3], [1])); 
+console.log(without(["1", "2", "3"], [1, 2, "3"]))
+console.log(without(["1", "2", "3", 0], ["1", 2, "3", 1])); 
