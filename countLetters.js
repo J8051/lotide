8353d6fbeb884444a2b6a9d8da0ffbb3ed3bd6cd
J8051 <nathaniel.jessica@yahoo.ca>
2022-11-20 18:24:43 -0500

    my function returns an object with the letters counted

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+
+const countLetters = function(stringPhrase){
+  result={
+
+  };
+    for(const letters of stringPhrase){
+      if(result[letters]){
+        result[letters]+=1;
+      }else{ 
+        result[letters]=1;
+      } 
+    }
+      
+ return result; 
+};
+
+console.log(countLetters("LHL")); 
+
+
