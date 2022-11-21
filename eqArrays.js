ee5629c7c463e14c4fa2a741a575a170a81ac121
J8051 <nathaniel.jessica@yahoo.ca>
2022-11-21 14:34:03 -0500

    eqArrays function fixed

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,21 @@
 
 const eqArrays = function(arrOne,arrTwo){
- return arrOne.toString() === arrTwo.toString(); 
-};
+let lengthArrOne = arrOne.length; 
+let lengthArrTwo = arrTwo.length;
+  if(lengthArrOne !== lengthArrTwo){
+    return false; 
+  }else{
+    for(let i=0; i<arrOne.length; i++){
+      if(arrOne[i] !== arrTwo[i]){
+        return false; 
+      }
+    }
+      }
+return true; 
+  };
 
-eqArrays([1, 2, 3], [3, 2, 1]); 
-eqArrays([1, 2, 3], [1, 2, 3]); 
-eqArrays(["1", "2", "3"], ["1", "2", "3"])
-eqArrays(["1", "2", "3"], ["1", "2", 3]) 
+
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); 
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); 
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); 
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));  
