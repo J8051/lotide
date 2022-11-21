c76727d2f60739cfe10ecd69c43a4a98552eac74
J8051 <nathaniel.jessica@yahoo.ca>
2022-11-21 15:50:56 -0500

    assertArrayEquals function is fixed

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,8 +1,29 @@
 const eqArrays = function(arrOne,arrTwo){
-  JSON.stringify(arrOne) === JSON.stringify(arrTwo) ? console.log(`✅Assertion Passed ${arrOne} is the same as ${arrTwo}`): console.log(`❌Assertion Failed: ${arrOne} is not the exactly same as ${arrTwo}`)
+  let lengthArrOne = arrOne.length; 
+  let lengthArrTwo = arrTwo.length;
+    if(lengthArrOne !== lengthArrTwo){
+      return false; 
+    }else{
+      for(let i=0; i<arrOne.length; i++){
+        if(arrOne[i] !== arrTwo[i]){
+          return false; 
+        }
+      }
+        }
+  return true; 
+    };
+    //eqArrays([1, 2, 3], [3, 2, 1]); 
+    //eqArrays([1, 2, 3], [1, 2, 3]); 
+    //eqArrays(["1", "2", "3"], ["1", "2", "3"]);
+    //eqArrays(["1", "2", "3"], ["1", "2", 3]); 
+
+const assertArrayEquals = function(arrOne,arrTwo){
+if(eqArrays(arrOne,arrTwo)){
+  console.log(`✅Assertion Passed ${arrOne} is the same as ${arrTwo}`)
+}else{
+  console.log(`❌Assertion Failed: ${arrOne} is not the exactly same as ${arrTwo}`)
+}
+
  };
  
- eqArrays([1, 2, 3], [3, 2, 1]); 
- eqArrays([1, 2, 3], [1, 2, 3]); 
- eqArrays(["1", "2", "3"], ["1", "2", "3"])
- eqArrays(["1", "2", "3"], ["1", "2", 3]) 
+console.log(assertArrayEquals([1, 2, 3], [3, 2, 1])); 
