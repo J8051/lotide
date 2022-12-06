a494b65a282e0d9be395f9732acaaae430714888
J8051 <nathaniel.jessica@yahoo.ca>
2022-12-06 08:12:52 -0500

    updated to include tail.js's test code

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,7 @@
 const assertEqual = require("../assertEqual"); 
 const head = require("../head"); 
+const tail = require("../tail"); 
+
 // TEST CODE AssertEqual 
 assertEqual("Bootcamp", "Bootcamp");
 assertEqual(1, 1);
@@ -8,3 +10,15 @@ assertEqual(9, 9);
 // TEST CODE Head 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// TEST CODE Tail 
+assertEqual(tail(["Hello","Lighthouse", "Labs"]).length,2);
+assertEqual(tail(["Hello","Lighthouse", "Labs"])[0],"Lighthouse");
+assertEqual(tail(["Hello","Lighthouse", "Labs"])[1],"Labs");
+
+//Test Case: check the original array
+
+tail(["YoYo","Lighthouse","Labs"]);
+assertEqual(["YoYo","Lighthouse","Labs"].length, 3);
+
+module.exports = tail; 
