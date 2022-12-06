dea8161812c46769ae4f182780777aef81488a39
J8051 <nathaniel.jessica@yahoo.ca>
2022-12-05 20:10:18 -0500

    assertEqual and head functions cleaned up

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,4 @@ const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`❌Assertion Failed: ${actual} !== ${expected} `)
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 9);
+module.exports = assertEqual; 
--- head.js
+++ head.js
@@ -1,4 +1,6 @@
-const head = function(arr) {
+const assertEqual = require("./assertEqual");
+
+const head = function (arr) {
   let firstElement;
   if (arr.length < 1) {
       firstElement = undefined;
@@ -11,10 +13,5 @@ const head = function(arr) {
   return firstElement;
 };
 
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`❌Assertion Failed: ${actual} !== ${expected} `)
-};
+module.exports = head; 
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
