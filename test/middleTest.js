3d5de59aedc6bea7af8d06866102d6a6e87f8cf6
J8051 <nathaniel.jessica@yahoo.ca>
2022-12-06 11:01:19 -0500

    testing middle.js with mocha & chai

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const assert = require("chai").assert; 
+const middle = require("../middle"); 
+
+describe("#middle", () => {
+
+    it("returns [] for middle([1])", () => {
+        assert.deepEqual(middle([1]),[]);
+    })
+    it("returns [2] for [1, 2, 3]", () => { 
+        assert.deepEqual(middle([1, 2, 3]), [2]); 
+    })
+}); 
